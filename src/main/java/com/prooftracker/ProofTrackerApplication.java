b3a6a239296ad8695ea7c2c7e054c474b11c7146
ratrahu007 <rahulrathod6624@gmail.com>
2026-06-17 19:04:21 +0530

    Initial project setup

---
+++ src/main/java/com/prooftracker/ProofTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.prooftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProofTrackerApplication.class, args);
+    }
+
+}
